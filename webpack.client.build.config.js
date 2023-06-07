a4a915a65fbe688230c83a305122ca28cf4da8cd
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-06-07 17:27:06 +0100

    server client file using express

--- webpack.client.build.config.js
+++ webpack.client.build.config.js
@@ -15,6 +15,15 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                  "file-loader",
+                  {
+                    loader: "image-webpack-loader",
+                  },
+                ],
+              },
+            {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
